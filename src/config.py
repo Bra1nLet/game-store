113e9f990e9a744179bb96a2cc8a64bf24ae0cb0
ostapenko bohdan <kvjn630@gmail.com>
2024-10-15 11:03:30 +0300

    production ready

--- src/config.py
+++ src/config.py
@@ -1,8 +1,13 @@
-MONGODB_CONNECTION_STRING = ""
-GAME_CATEGORIES_FILE_PATH = ""
-SUBSCRIBE_CATEGORIES_FILE_PATH = ""
-BOT_TOKEN = ""
+MONGODB_CONNECTION_STRING = "mongodb://root:example@localhost:27017"
+GAME_CATEGORIES_FILE_PATH = "/src/migrations/innitial_data/game-categories"
+SUBSCRIBE_CATEGORIES_FILE_PATH = "/src/migrations/innitial_data/subscribe-categories"
+BOT_TOKEN = "7375016958:AAHg8nHRVIYxyoLrYJhRtij-5iQNLj8zE3Q"
 
 PROXY_LIST = [
-
+    "http://102nvcf3ym:PGc5ecQjaThGotrcS@93.190.141.157:15105",
+    "http://102nvcf3ym:PGc5ecQjaThGotrcS@93.190.141.157:20689",
+    "http://102nvcf3ym:PGc5ecQjaThGotrcS@93.190.141.157:19518",
+    "http://102nvcf3ym:PGc5ecQjaThGotrcS@109.236.80.35:40897",
+    "http://102nvcf3ym:PGc5ecQjaThGotrcS@109.236.80.35:27190",
+    "http://102nvcf3ym:PGc5ecQjaThGotrcS@109.236.80.35:48455",
 ]
--- src/digital_store/tgbot/utils/media/edit_pic.py
+++
@@ -1,4 +0,0 @@
-from PIL import Image
-
-image = Image.open('description.png').resize((640, 360))
-image.save('description.png')
